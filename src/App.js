c4fe87faec0047f9d9f742cce4b262d22666bb4e
Margarita Fiodorova <breathles@mail.ru>
2021-06-02 02:49:20 +0300

    Change loader color

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
 
 const octokit = new Octokit();
+const SKY_BLUE = '#00b4eb';
 
 function App() {
   const loadingRef = useRef(null);
@@ -32,7 +33,7 @@ function App() {
 
   return (
     <div className="layout">
-      <LoadingBar color="#fff" height={3} ref={loadingRef} />
+      <LoadingBar color={SKY_BLUE} height={3} ref={loadingRef} />
       <Header fetchUser={fetchUser} />
       <div className="content">
         <Sidebar user={user} />
